bd92da722425508185839850815a6560e8964a31
George Cook <george@tantawowa.com>
2021-01-21 18:04:42 +0100

    improves robustness if errors are encountered

--- src/plugin.ts
+++ src/plugin.ts
@@ -13,7 +13,7 @@ let rokuLogConfig: {
 };
 
 class RokuLogPlugin implements CompilerPlugin {
-  public name = 'login-plugin';
+  public name = 'log-plugin';
 
   beforeProgramCreate(builder: ProgramBuilder): void {
     rokuLogConfig = (builder.options as any).rokuLog || {
@@ -32,30 +32,37 @@ class RokuLogPlugin implements CompilerPlugin {
   beforeFileTranspile(entry: TranspileObj) {
     if (isBrsFile(entry.file)) {
       const parser = entry.file.parser;
+      // console.log('>>>', entry.file);
       for (let expr of parser.references.functionExpressions) {
         expr.body.walk(createVisitor({
           ExpressionStatement: (es) => {
+
             let ce = es.expression as CallExpression;
             if (ce) {
               let ve = ce.callee as VariableExpression;
               if (ve) {
                 const logMethodRegex = /log(error|warn|info|method|verbose|debug)/i;
-                if (logMethodRegex.test(ve.name.text)) {
-                  if (rokuLogConfig.strip) {
-                    return new EmptyStatement();
-                  } else if (rokuLogConfig.insertPkgPath) {
-                    const t = createToken(TokenKind.SourceLocationLiteral, '', ce.range);
-                    var sourceExpression = new SourceLiteralExpression(t);
-                    if (ce.args.length > 0) {
+                try {
+                  if (ve.name && logMethodRegex.test(ve.name.text)) {
+                    if (rokuLogConfig.strip) {
+                      return new EmptyStatement();
+                    } else if (rokuLogConfig.insertPkgPath) {
+                      const t = createToken(TokenKind.SourceLocationLiteral, '', ce.range);
+                      var sourceExpression = new SourceLiteralExpression(t);
 
-                      ce.args[0] = new BinaryExpression(sourceExpression, createToken(TokenKind.Plus, '+ " " + ', ce.range), ce.args[0]);
-                    } else {
-                      ce.args.push(sourceExpression);
+                      if (ce.args.length > 0) {
+                        ce.args[0] = new BinaryExpression(sourceExpression, createToken(TokenKind.Plus, '+ " " + ', ce.range), ce.args[0]);
+                      } else {
+                        ce.args.push(sourceExpression);
+                      }
                     }
                   }
+                } catch (e) {
+                  console.log(`Error parsing file: ${entry.file.pkgPath} ${e.getMessage()}`);
                 }
               }
             }
+            return es;
           }
         }), { walkMode: WalkMode.visitAllRecursive });
       }
